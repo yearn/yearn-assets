b3f275eb726e5fdeb3def2ba97ba4cd654c6cbfd
Major <90963895+Major-Eth@users.noreply.github.com>
2022-02-23 18:49:56 +0100

    feat(package): creation of yearn-web-assets package (#195)
    fix(package): update package.json config
    
    feat(package): add options to improve customization
    
    Possibility to change className, style and width/height
    
    fix(package): add catch system to avoid crash for bad addr
    
    doc(package): update install doc

---
+++ yearn-web-assets/example/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import Token from 'yearn-web-tokens'
+
+const App = () => {
+  return (
+    <div>
+      <Token
+        format={'svg'}
+        address={'0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9'}
+        onClick={() => alert(`Hello Web3`)}
+        style={{cursor: 'pointer'}}
+        chainID={'42161'} />
+      <Token
+        format={'svg'}
+        address={'0x0d4EA8536F9A13e4FBa16042a46c30f092b06aA5'}
+        width={48}
+        height={48}
+        chainID={'1'} />
+        <Token
+        format={'img'}
+        address={'0x1AEf73d49Dedc4b1778d0706583995958Dc862e6'}
+        chainID={'1'}
+        imgSize={'32'} />
+        <Token
+        format={'img'}
+        address={'0x0FCDAeDFb8A7DfDa2e9838564c5A1665d856AFDF'}
+        width={48}
+        height={48}
+        chainID={'250'} />
+        <Token
+        format={'img'}
+        address={'0x0FCDAeDFb8A7DfDa2e9838564c5A1665d856AFeF'}
+        width={48}
+        height={48}
+        chainID={'250'} />
+      </div>
+  );
+}
+
+export default App
